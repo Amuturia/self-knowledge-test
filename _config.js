4597253dd255d7623c1cdd0c0dd4900f88e99ebf
amuturia <antony.muturia@student.morningaschool.com>
2024-07-08 19:44:02 +0300

    Completed npm test.

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Amuturia:Apollemi.30@amcluster.ja8zp7r.mongodb.net/arkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Amuturia:Apollemi.30@amcluster.ja8zp7r.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Amuturia:Apollemi.30@amcluster.ja8zp7r.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
